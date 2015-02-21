c4f5ac4e4592e5c262a6c4f347c510dcff4e5ee0
Lewis Menelaws <lmenelaws112@gmail.com>
2015-02-21 04:38:05 -0500

    Added Chart

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express'});
 });
 
 //router.get('/test', function(req, res, next) {
