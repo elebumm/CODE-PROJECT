6a3e995c77f4b0bec3fc29900b34cff05e50ddb1
Jasmyn Newton <jasmyn.n.newton@gmail.com>
2015-02-22 09:14:02 -0500

    Removed chart scripts from About, Contact and Index pages. Updated index.js and results.js with routes.

--- routes/results.js
+++ routes/results.js
@@ -1,25 +1,40 @@
 var express = require('express');
 var router = express.Router();
-var projections = require('../models/emp_projections');
 
 /* GET contact page. */
 router.get('/', function(req, res, next) {
-    res.render('results', {title: 'ContactDream Results'});
+    res.render('results', {title: 'DreamJob Results'});
 });
 
-router.get('/chartDemo/:id', function (req, res, next) {
+/* ------------- COPS OCCUPATIONAL GROUPING -------------------- */
 
-    //console.log(req.params.id);
-    //console.log(typeof(req.params.id));
+// All Employment Categories and Respective Sub-Categories
+router.get('/jobCat', function(req, res, next) {
+    res.render('results', {title: 'DreamJob Results', jobs: array});
+})
 
-    var x = req.params.id;
+/* --------------- JOB VACANCY STATISTICS ---------------------- */
 
-    console.log(x);
-    console.log(typeof(x));
+// All Annual Job Vacancies by Year
+router.get('/annJobVacYr/:year', function(req, res, next) {
+    res.render('results', {title: 'DreamJob Results', years: array, numVacancies: array});
+})
 
-    projections.find({ ID : req.params.id }, function (err, docs) {
-        res.json(docs);
-    });
-});
+// All Annual Job Vacancies for Specified ID
+router.get('/annJobVacID/:jobID', function(req, res, next) {
+    res.render('results', {title: 'DreamJob Results', job: name, numVacancies: array});
+})
+
+// All Annual Job Vacancies for Specified ID for Specified Year
+router.get('/annJobVacYrID/:jobID/:year', function(req, res, next) {
+    res.render('results', {title: 'DreamJob Results', job: name, year: year, numVacancies: array});
+})
+
+// All Annual Job Vacancies for Specified ID for Year Range
+router.get('/annJobVacYrIDRan/:jobID/:startYear/:endYear', function(req, res, next) {
+    res.render('results', {title: 'DreamJob Results', job: name, years: array, numVacancies: array});
+})
+
 
 module.exports = router;
+
