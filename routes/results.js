f985ea73e86804b868df3d773a6b9ee16a7bd268
Jasmyn Newton <jasmyn.n.newton@gmail.com>
2015-02-22 09:49:29 -0500

    If this doesn't work I don't know what will.

--- routes/results.js
+++ routes/results.js
@@ -11,29 +11,29 @@ router.get('/', function(req, res, next) {
 // All Employment Categories and Respective Sub-Categories
 router.get('/jobCat', function(req, res, next) {
     res.render('results', {title: 'DreamJob Results', jobs: array});
-})
+});
 
 /* --------------- JOB VACANCY STATISTICS ---------------------- */
 
 // All Annual Job Vacancies by Year
 router.get('/annJobVacYr/:year', function(req, res, next) {
     res.render('results', {title: 'DreamJob Results', years: array, numVacancies: array});
-})
+});
 
 // All Annual Job Vacancies for Specified ID
 router.get('/annJobVacID/:jobID', function(req, res, next) {
     res.render('results', {title: 'DreamJob Results', job: name, numVacancies: array});
-})
+});
 
 // All Annual Job Vacancies for Specified ID for Specified Year
 router.get('/annJobVacYrID/:jobID/:year', function(req, res, next) {
     res.render('results', {title: 'DreamJob Results', job: name, year: year, numVacancies: array});
-})
+});
 
 // All Annual Job Vacancies for Specified ID for Year Range
 router.get('/annJobVacYrIDRan/:jobID/:startYear/:endYear', function(req, res, next) {
     res.render('results', {title: 'DreamJob Results', job: name, years: array, numVacancies: array});
-})
+});
 
 
 module.exports = router;
