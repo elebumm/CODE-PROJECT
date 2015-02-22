4ad0e88b43052026ffab061a29a593f012d1ad86
Jasmyn Newton <jasmyn.n.newton@gmail.com>
2015-02-21 23:37:08 -0500

    Updated Logo, Index and About Pages.

--- models/grouping.js
+++ models/grouping.js
@@ -1,17 +1,17 @@
 /**
  * Created by Steven Lambe on 2/21/15.
  */
-var db = require('../db');
-
-var SubcategoriesSchema = new Schema({
-    ID : String,
-    Name : String
-});
-
-var Grouping = db.model('Grouping', {
-    catID : { type : String, required : true },
-    catName : { type : String, required : true },
-    subcategories : [SubcategoriesSchema]
-});
-
-module.exports = Grouping;
+//var db = require('../db');
+//
+//var SubcategoriesSchema = new Schema({
+//    ID : String,
+//    Name : String
+//});
+//
+//var Grouping = db.model('Grouping', {
+//    catID : { type : String, required : true },
+//    catName : { type : String, required : true },
+//    subcategories : [SubcategoriesSchema]
+//});
+//
+//module.exports = Grouping;
