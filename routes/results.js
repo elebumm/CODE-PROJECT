f3f32df6bc95f92780bb9bb6c2b90edf598f0dd8
Dylan Huculak <dylan@dylanweb.ca>
2015-02-22 09:02:16 -0500

    Added methods in results.js for Employment Projections, Retirement Projections, and Summary of Cumulative Job Openings And Job Seekers Over The Projection Period

--- routes/results.js
+++ routes/results.js
@@ -3,7 +3,74 @@ var router = express.Router();
 
 /* GET contact page. */
 router.get('/', function(req, res, next) {
-    res.render('results', {title: 'ContactDream Results'});
+    res.render('results', {title: 'DreamJob Results'});
 });
+
+// ANNUAL EMPLOYMENT PROJECTIONS
+// ----------------------------------------------------------------------------------------
+
+// Annual Employment Projections All Data
+router.get('/annEmpProAll/', function(req, res, next) {
+    var value = {
+
+    }
+
+    res.render('results', {title: 'DreamJob Results', results: value});
+})
+
+// Annual Employment Projections Using Year Range (All Job Categories)
+router.get('/annEmpProRan/:startYear/:endYear', function(req, res, next) {
+    res.render('results', {title: 'DreamJob Results', startYear: value, endYear: value});
+})
+
+// Annual Employment Projections Using Job Category (All years)
+router.get('/annEmpProCat/:cat/startYear/:endYear', function(req, res, next) {
+    res.render('results', {title: 'DreamJob Results', jobCat: value});
+})
+
+// Annual Employment Projections Using Job Category AND Year Range
+router.get('/annEmpProCatRan/:cat/startYear/:endYear', function(req, res, next) {
+    res.render('results', {title: 'DreamJob Results', jobCat: value, startYear: value, endYear: value});
+})
+
+// REPLACEMENT DEMAND IN THE FORM OF ANNUAL RETIREMENT
+// ---------------------------------------------------------------------------------------
+
+// Annual Retirement Projections All Data
+router.get('/annRetProAll/', function(req, res, next) {
+    res.render('results', {title: 'DreamJob Results', results: value});
+})
+
+// Annual Retirement Projections Using Year Range (All Job Categories)
+router.get('/annRetProRan/:startYear/:endYear', function(req, res, next) {
+    res.render('results', {title: 'DreamJob Results', startYear: value, endYear: value});
+})
+
+// Annual Retirement Projections Using Job Category (All years)
+router.get('/annRetProCat/:cat/startYear/:endYear', function(req, res, next) {
+    res.render('results', {title: 'DreamJob Results', jobCat: value});
+})
+
+// Annual Retirement Projections Using Job Category AND Year Range
+router.get('/annRetProCatRan/:cat/startYear/:endYear', function(req, res, next) {
+    res.render('results', {title: 'DreamJob Results', jobCat: value, startYear: value, endYear: value});
+})
+
+// SUMMARY OF CUMULATIVE JOB OPENINGS AND JOB SEEKRS
+// ---------------------------------------------------------------------------------------
+// URL args:
+//  The first two are Job Category, and Employment in 2012.
+//  The rest cover 2013-2022 and are as follows:
+//      Employment Growth, Retirements, Other Replacement Demand, Job Openings,
+//      Job Seekers (School Leavers), Job Seekers (Immigrants), Job Seekers (Other Categories), Job Seekers (All Combined),
+//  The last two are listed as either Balance, Shortage, OR Surplus and are:
+//      Assessment of Labour Market Conditions (from 2010-2012), AND
+//      Projected Final Assessment of Labour Market Conditions (from 2013-2022)
+// NOTE: The last column, and the 3rd last column in the dataset are French, and will not be used.
+router.get('/openingsAndSeekers/:cat/:emp2012/:empGrow/:ret/:othOpen/:allOpen/:school/:immi/:othSeek/:allSeek/:lmdAss/:lmdProj'), function(req, res, next) {
+    res.render('results', {title: 'DreamJob Results', jobCat: value, columns: obj});
+})
+
+
 
 module.exports = router;
