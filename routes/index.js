2bf5e1e58ac55b69c4be0f2c564ac6e6ca5749dc
firrae <firrae@gmail.com>
2015-02-22 05:26:40 -0500

    fixed imports and corrected employee projections.

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ var fs = require('fs');
 var request = require('request');
 var Grouping = require('../models/grouping');
 var Summary = require('../models/summary');
+var retirement = require('../models/retirement');
+var projections = require('../models/emp_projections');
 
 var endOfLine = require('os').EOL;
 
@@ -206,7 +208,7 @@ router.get('/dataPull', function(req, res, next) {
 
             for(var i = 0; i < elements.length - 1; i++)
             {
-                csv += "," + (2012 + i);
+                csv += ",r_" + (2012 + i);
             }
 
             csv += endOfLine;
@@ -226,12 +228,27 @@ router.get('/dataPull', function(req, res, next) {
                     });
                 };
 
-                //var counter = 0;
-                //
-                //for(var i = 0; i < json.length; i++)
-                //{
-                //    counter++;
-                //}
+                for (var i = 0; i < json.length; i++) {
+                    if (typeof(json[i].nCode) != "undefined") {
+                        var group = new projections({
+                            ID: json[i].nCode,
+                            year1: json[i].r_2013,
+                            year2: json[i].r_2014,
+                            year3: json[i].r_2015,
+                            year4: json[i].r_2016,
+                            year5: json[i].r_2017,
+                            year6: json[i].r_2018,
+                            year7: json[i].r_2019,
+                            year8: json[i].r_2020,
+                            year9: json[i].r_2021,
+                            year10: json[i].r_2022
+                        });
+
+                        group.save();
+                        console.log("Item " + i + " insterted.");
+                        //console.log(finalJSON[i]);
+                    }
+                }
 
                 res.json(json);
                 console.log(typeof json);
