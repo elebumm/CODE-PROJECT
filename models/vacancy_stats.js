74ddebcf37b47d7b397674fec8f9e1d77a19d230
Lewis Menelaws <lmenelaws112@gmail.com>
2015-02-22 02:32:16 -0500

    Updated Models folder

--- models/vacancy_stats.js
+++ models/vacancy_stats.js
@@ -6,13 +6,13 @@ var mongoose = require('mongoose');
 var db = require('../db');
 
 var vacancy_stats = db.model('vacancy_stats', {
-    ref_date : {type : double, required : true },
+    ref_date : {type : Number, required : true },
     geo : {type : String, required : true},
     stats : {type : String, required : true},
     naics : {type : String, required : true},
     vector : {type : String, required : true},
     coordinate : {type : String, required : true},
-    value : {type : double, required : true}
+    value : {type : String }
 });
 
 module.exports = vacancy_stats;
