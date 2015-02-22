bfba3d40a1c52eb98a5ef9d1e8cd6d783da2910b
Jasmyn Newton <jasmyn.n.newton@gmail.com>
2015-02-22 17:04:33 -0500

    Merging things and such.

--- routes/results.js
+++ routes/results.js
@@ -31,17 +31,17 @@ router.get('/jobCat', function(req, res, next) {
 /* --------------- JOB VACANCY STATISTICS ---------------------- */
 
 // All Annual Job Vacancies by Year
-router.get('/annJobVacYr/:year', function(req, res, next) {
+router.get('/annJobVac/:year', function(req, res, next) {
     res.render('results', {title: 'DreamJob Results', years: array, numVacancies: array});
 });
 
 // All Annual Job Vacancies for Specified ID
-router.get('/annJobVacID/:jobID', function(req, res, next) {
+router.get('/annJobVac/:jobID', function(req, res, next) {
     res.render('results', {title: 'DreamJob Results', job: name, numVacancies: array});
 });
 
 // All Annual Job Vacancies for Specified ID for Specified Year
-router.get('/annJobVacYrID/:jobID/:year', function(req, res, next) {
+router.get('/annJobVac/:jobID/:year', function(req, res, next) {
     res.render('results', {title: 'DreamJob Results', job: name, year: year, numVacancies: array});
 });
 
